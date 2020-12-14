353d6ca9515c9618bb6937117c7a7bac20804c27
서민규 <alsrs3679@naver.com>
2020-12-14 17:30:18 +0900

    프로젝트 초기파일

---
+++ src/main/java/com/jejuda/JejudaApplication.java
@@ -0,0 +1,13 @@
+package com.jejuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JejudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JejudaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jejuda/controller/MainController.java
@@ -0,0 +1,72 @@
+package com.jejuda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+	
+	@GetMapping(value="/")
+	public String mvindex() {
+		return "/index";
+	}
+	
+	@GetMapping(value="/shoppingcart")
+	public String shoppingcart() {
+		return "/shopping-cart";
+	}
+	
+	@GetMapping(value="/delivery")
+	public String delivery() {
+		return "/delivery";
+	}
+	
+	@GetMapping(value="/seaproduct")
+	public String seaproduct() {
+		return "/shop-grid";
+	}
+	
+	@GetMapping(value="/fruit")
+	public String fruit() {
+		return "/shop-grid";
+	}
+	
+	@GetMapping(value="/pig")
+	public String pig() {
+		return "/shop-grid";
+	}
+	
+	@GetMapping(value="/order")
+	public String order() {
+		return "/checkout";
+	}
+	
+	@GetMapping(value="/board")
+	public String board() {
+		return "/board";
+	}
+	
+	@GetMapping(value="/qna")
+	public String qna() {
+		return "/board";
+	}
+	
+	@GetMapping(value="/contact")
+	public String contact() {
+		return "/contact";
+	}
+	
+	@GetMapping(value="/seaproduct/fish/1")
+	public String fish_detail() {
+		return "/shop-details";
+	}
+	
+	
+
+	
+	
+	
+
+}
